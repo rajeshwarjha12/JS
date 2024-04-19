6964117add71986c618c96e58c5eaa68bddefbde
rajeshwarjha/12 <jharajeshwar1998@gmail.com>
2024-04-20 00:45:20 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+
+
+let name = "shanu"
+let age = 21 
+let isLogged = false
+let state =  null
+
+
+//number => 2 to power 53
+//bigint
+//string =>
+//boolean => true/false
+//null=> standalone value
+//undefined => 
+//symbol=> unique
+
+
+//object
+
+console.log(typeof"shanu");//string
---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 7367
+let accountEmail = "shanu@google.com"
+var accountPassword = "12345 "
+accountCity = "madhubani"
+let accountState;
+
+//accountId = 2 // not allowed
+accountEmail = "hafvhan"
+accountPassword = "547"
+accountCity = "dbg"
+
+console.log("accountId");
+
+
+//prefer not to use var because of issue in bloc scope functional scope
+
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity ,accountState])
+
