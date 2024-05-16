970808de03f4b9e0a6de103b9305ae06549e6173
Rajeswhar Jha <124435018+rajeshwarjha12@users.noreply.github.com>
2024-05-17 01:34:42 +0530

    conversion of datatypes

--- 01_basics/3rd_conversionOperation.js
+++ 01_basics/3rd_conversionOperation.js
@@ -18,3 +18,49 @@ let booleanIsLoggedIn = Boolean (isLoggedIn);
 //1 => true; 0 => false
 //""=> false
 //"shanu"=> true
+
+
+//*****************OPERATIONS****************
+
+let value = 3
+let negValue = -value
+console.log(negValue) 
+// => -3
+
+//console.log(3+2);
+//console.log(3-2);
+//console.log(2*2);
+//console.log(3**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "shanu"
+
+let str3 = str1+str2
+console.log(str3);
+//=>hello+shanu
+
+console.log("1"+2);
+// => 12
+console.log(1 + "2");
+//=>12
+console.log("1"+2+2);
+//=> 122
+console.log(1+2+"2");
+// => 32
+
+console.log(+true);
+// => true
+console.log(true+);
+// => error
+
+let gameCounter = 100
+gameCounter++;
+console.log("gameCounter");
+// => 101
+
+let gameCounter = 100
+++gameCounter;
+console.log("gameCounter");
+// => 101
