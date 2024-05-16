fd1f351f82891f34ceae4d931a87e3925029bc62
Rajeswhar Jha <124435018+rajeshwarjha12@users.noreply.github.com>
2024-05-17 00:54:42 +0530

    Conversion of datatypes

--- 01_basics/3rd_conversionOperation.js
+++ 01_basics/3rd_conversionOperation.js
@@ -3,3 +3,18 @@ let scrore = 33
 console.log(typeof score);
 console.log(typeof (score));
  
+let valueInNumber = Number(scrore)
+console.log(typeof valueInNumber);
+console.log(valueInNumber) ;
+
+// "33"=> 33
+// "33abc"=> Nan
+//true => 1;false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+
+//1 => true; 0 => false
+//""=> false
+//"shanu"=> true
